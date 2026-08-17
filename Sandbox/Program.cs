85ecebfe7b23004230062aad7f3bc1f529a0e229
fabito <al.20242260@umq.maristas.edu.mx>
2026-08-17 10:56:44 -0600

    Banderas :)))

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,10 +6,7 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-
-
-            Console.Write("\n\n\n");
-            // Generar un programa que muestre atributos...
+            // Estadísticas de Greg El Mago
             Console.WriteLine("Estadisticas en forma de tabla");
             Console.WriteLine("/ Greg \"El Mago\" /");
             Console.WriteLine("\\------------------\\");
@@ -17,9 +14,260 @@ namespace Sandbox
             Console.WriteLine("\\ MP\t700        \\");
             Console.WriteLine("/ Gold\t8000        /");
             Console.WriteLine("\\ Exp\t25000       \\");
+
+            // Bandera de México
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de México");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("ME");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("XI");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("CO\n");
+
+            Console.ResetColor();
+
+            Console.Write("\n\n\n");
+            Console.WriteLine("Bandera de Mexico");
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("ME");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("XI");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("CO");
+
+            Console.ResetColor();
+
+            // Bandera de Curazao
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Curazao");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("CU");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("R");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("AZ");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("AO\n");
+
+            Console.ResetColor();
+
+            // Bandera de Tanzania
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Tanzania");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("TA");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("N");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("ZA");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("N");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("IA");
+
+            Console.ResetColor();
+
+            // Bandera de Kazajistan
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Kazajistan");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("K");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("AZA");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("JI");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("STAN");
+
+            Console.ResetColor();
+
+            // Bandera de Seychelles
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Seychelles");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("SE");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("YC");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("HE");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("LL");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("ES");
+
+            Console.ResetColor();
+
+            // Bandera de Moldavia
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Moldavia");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("MO");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("L");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("DA");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("V");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("IA");
+
+            Console.ResetColor();
+
+            // BANDERAS CON FONDO
+
+            // Bandera de Curazao
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Curazao");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("CU");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("R");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("AZ");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("AO");
+
+            Console.ResetColor();
+
+            // Bandera de Tanzania
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Tanzania");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("TA");
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("N");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("ZA");
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("N");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("IA");
+
+            Console.ResetColor();
+
+            // Bandera de Kazajistan
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Kazajistan");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("K");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("AZA");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("JI");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("STAN");
+
+            Console.ResetColor();
+
+            // Bandera de Seychelles
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Seychelles");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("SE");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("YC");
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("HE");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("LL");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("ES");
+
+            Console.ResetColor();
+
+            // Bandera de Moldavia
+            Console.Write("\n\n\n"); // Saltos de línea para separar
+            Console.WriteLine("Bandera de Moldavia");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("MO");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("L");
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("DA");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("V");
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("IA");
+
+            Console.ResetColor();
         }
     }
 }
-
-
 
