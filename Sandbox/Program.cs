df87fdf9caae504d15dbd872063bdb22ed2d08d1
fabito <al.20242260@umq.maristas.edu.mx>
2026-08-17 13:51:43 -0600

    Guía añadida

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,7 +9,7 @@ namespace Sandbox
             // Estadísticas de Greg El Mago
             Console.WriteLine("Estadisticas en forma de tabla");
             Console.WriteLine("/ Greg \"El Mago\" /");
-            Console.WriteLine("\\------------------\\");
+            Console.WriteLine("\\-----------------\\");
             Console.WriteLine("/ HP\t200        /");
             Console.WriteLine("\\ MP\t700        \\");
             Console.WriteLine("/ Gold\t8000        /");
