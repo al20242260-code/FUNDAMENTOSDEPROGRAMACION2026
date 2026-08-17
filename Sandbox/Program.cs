e5fb35e114402cd55321bbe1655baf8eafa57228
fabito <al.20242260@umq.maristas.edu.mx>
2026-08-16 23:11:42 -0600

    Poner mi nombre #esfuerzo #cr7

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,9 +1,14 @@
+using System;
+
 namespace Sandbox
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("hola");
+            Console.Write(" ");
+            Console.Write("fabito!");
         }
     }
 }
