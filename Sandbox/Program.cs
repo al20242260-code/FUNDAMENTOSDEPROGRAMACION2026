fec8f7f80f1a0f3f22a1a5e494f93f1844d3f057
fabito <al.20242260@umq.maristas.edu.mx>
2026-08-17 09:58:32 -0600

    Ejercicio: Actualización de los stats del ejercicio con Control Write y Control WriteLine.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -12,9 +12,11 @@ namespace Sandbox
             // Generar un programa que muestre atributos...
             Console.WriteLine("Estadisticas en forma de tabla");
             Console.WriteLine("/ Greg \"El Mago\" /");
-            Console.WriteLine("------------------");
+            Console.WriteLine("\\------------------\\");
             Console.WriteLine("/ HP\t200        /");
-            Console.WriteLine("/ MP\t700        /");
+            Console.WriteLine("\\ MP\t700        \\");
+            Console.WriteLine("/ Gold\t8000        /");
+            Console.WriteLine("\\ Exp\t25000       \\");
         }
     }
 }
