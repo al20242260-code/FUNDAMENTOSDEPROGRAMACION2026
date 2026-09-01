da9e232f61b28c1afebfda12f680cedec14f47be
fabito <al.20242260@umq.maristas.edu.mx>
2026-09-01 00:09:21 -0600

    ULTIMA ACTUALIZACION

---
+++ 26-08-26/Program.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace _26_08_26
+{
+    internal class Program
+    {
+        public static void Main(string[] args) {
+            // See https://aka.ms/new-console-template for more information
+
+
+            Console.WriteLine("Hello, World!");
+
+
+// Establecer un programa que revise el pago de deudas a resolver y cuales se pueden resolver con el dinero disponible.
+// Si tengo 40000 puedo pagar todas las tres deudas, si tengo 20000 solo puedo pagar dos, si tengo 10000 solo puedo pagar una, y si tengo menos no puedo pagar ninguna.
+
+
+// Establecer un programa que dada una calificación sea aprobatoria o reprobatoria.
+
+
+            int Calif1 = 9;
+            if (Calif1 >= 9) {
+                Console.WriteLine("Aprobado");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Reprobado");
+            }
+
+        }
+    }
+}
